eaf3b262ee43110c7944e4779e25059fd3c7e388
Shakedflam <shakedflam@gmail.com>
2024-11-05 16:54:06 +0200

    Maybe fvcalc will upload

--- Bill3.java
+++ Bill3.java
@@ -2,10 +2,10 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-		String name2=args[1];
-        String name3=args[2];
-        int sum= Integer.parseInt(args[3]);
+	    String name1 = "Dan";
+		String name2= "lisa";
+        String name3= "Ron";
+        int sum= 100;
         double pay= Math.ceil(sum/(double)3);
         String conclude= "Dear "+name1+", "+name2+" and "+name3+": pay "+pay+" Shekels each.";
         System.out.println(conclude);	
