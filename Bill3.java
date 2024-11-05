bc383e9deb8f88ee8ee7d5aac75bbf06331f2f2f
Shakedflam <shakedflam@gmail.com>
2024-11-05 16:26:41 +0200

    hello

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,11 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2=args[1];
+        String name3=args[2];
+        int sum= Integer.parseInt(args[3]);
+        double pay= Math.ceil(sum/(double)3);
+        String conclude= "Dear "+name1+", "+name2+" and "+name3+": pay "+pay+" Shekels each.";
+        System.out.println(conclude);	
 	}
 }
